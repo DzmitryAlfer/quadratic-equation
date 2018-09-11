b1aec073a441975a5b3aa7e21f4b27880eb89649
da <>
2018-09-11 17:27:57 +0300

    Add implementation of solveEquation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  const aStr = aRegex.exec(equation)[1].replace(/ /g,'');
+  const bStr = bRegex.exec(equation)[1].replace(/ /g,'');
+  const cStr = cRegex.exec(equation)[1].replace(/ /g,'');
+
+  return solve(parseInt(aStr), parseInt(bStr), parseInt(cStr));
+}
+
+const aRegex = /([+-]? *[\d.]+) *\*{1} *x\^2/
+const bRegex = /([+-]? *[\d.]+) *\*{1} *x(?!\^)/
+const cRegex = /([+-]? *[\d.]+)$/
+
+function solve(a, b, c) {
+  var x1 = Math.round(-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+  var x2 = Math.round(-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+
+  return [x1,x2].sort((a, b) => { return a - b;});
 }
